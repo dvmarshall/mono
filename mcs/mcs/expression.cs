9bbc3e3d68c76905689b1a28f8b1c38e955c4658
David Marshall <djmarsha@annlnxdjmarsha14.(none)>
2015-06-25 08:30:34 -0400

    This check needs to be disabled in order to compiler against the Microsoft Office interop.   Calls to these "set_" and "get_" methods are perfectly legal in .NET, but fail in mono because of this check.

--- mcs/mcs/expression.cs
+++ mcs/mcs/expression.cs
@@ -7137,7 +7137,7 @@ namespace Mono.CSharp
 				return null;
 			}
 
-			IsSpecialMethodInvocation (ec, method, loc);
+			//IsSpecialMethodInvocation (ec, method, loc);
 			
 			eclass = ExprClass.Value;
 			return this;
