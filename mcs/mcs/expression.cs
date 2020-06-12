4bfdc060d364f6672fb28d13402be62f59233896
Qihong Wang <qwang@cdcc7bldpoolv25.ansys.com>
2020-06-11 22:33:23 -0400

    Apply changes in 4.8 and 5.20 part3

--- mcs/mcs/expression.cs
+++ mcs/mcs/expression.cs
@@ -7374,7 +7374,7 @@ namespace Mono.CSharp
 				return null;
 			}
 
-			IsSpecialMethodInvocation (ec, method, loc);
+	//AnsysPatch    IsSpecialMethodInvocation (ec, method, loc);
 			
 			eclass = ExprClass.Value;
 
