321c889f54fa4ceb4cb0834ebdef118ac28c3741
Qihong Wang <qwang@chqs12qwang.ansys.com>
2020-10-28 09:58:41 -0400

    handle NULL method in QK_CIT_BUILD2_019 and the other CIT tests

--- mono/metadata/reflection.c
+++ mono/metadata/reflection.c
@@ -614,6 +614,7 @@ method_object_construct (MonoDomain *domain, MonoClass *refclass, MonoMethod *me
 {
 	error_init (error);
 	g_assert (refclass != NULL);
+        goto_if (method == NULL, fail);
 	/*
 	 * We use the same C representation for methods and constructors, but the type 
 	 * name in C# is different.
